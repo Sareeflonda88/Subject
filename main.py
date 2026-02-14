e3c503185cae1572744a868c5113d0657ae96f12
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2026-02-15 01:19:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ OWNER_ID = 6644859358 # Replace with the actual owner's user ID
 # List of sudo users (initially empty or pre-populated)
 SUDO_USERS = [7897320272,1265397721,6644859358]
 
-AUTH_CHANNEL = -1003883944443
+AUTH_CHANNEL = -1003706016933
 
 # Function to check if a user is authorized
 def is_authorized(user_id: int) -> bool:
